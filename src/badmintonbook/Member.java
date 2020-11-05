a45179e010de5b0b0541aca87a3fde2ddaeb89a0
Thunnavat Chudum <thunnavat.c@mail.kmutt.ac.th>
2020-11-05 15:09:19 +0700

    fix bug

--- src/badmintonbook/Member.java
+++ src/badmintonbook/Member.java
@@ -12,18 +12,7 @@ import java.util.Scanner;
  * @author Student
  */
 public class Member {
-<<<<<<< HEAD
-    private  String name;
-    private  String telnum;
-    private Member[] members;
-    private int count;
 
-    
-    public Member(String name,String telnum){
-        this.name=name;
-        this.telnum=telnum;
-        this.members=new Member[100];
-=======
 
     private String name;
     private String telnum;
@@ -31,7 +20,7 @@ public class Member {
     public Member(String name, String telnum) {
         this.name = name;
         this.telnum = telnum;
->>>>>>> d2b4a4fc42365b5cf7ae78cb549eaac32cc4ba8f
+
 
     }
 
@@ -42,40 +31,8 @@ public class Member {
     public String getTelnum() {
         return telnum;
     }
-<<<<<<< HEAD
-//    public void register(Scanner input){
-//        
-//        if(count<members.length){
-//            members[count++] = new Member(getNameFromKeyboard(input),getTelNumFromKeyboard(input));
-//        }else {
-//            Member[] neoMember = new Member[members.length+100];
-//            for (int i = 0;i<members.length;i++){
-//                neoMember[i]=members[i];
-//                members=neoMember;
-//                members[count++] = new Member(getNameFromKeyboard(input),getTelNumFromKeyboard(input));
-//            }
-//        }
-//            
-//            
-//    }
-    public Member[] getMembers() {
-        return members;
-    }
-//    public String getNameFromKeyboard(Scanner input){
-//        System.out.println("Input name: ");
-//        return input.next();
-//    }
-//    public String getTelNumFromKeyboard(Scanner input){
-//        System.out.println("Input Tel: ");
-//        return input.next();
-=======
-//    public void register0(Scanner input){
-//        System.out.print("Input your Name : ");
-//        name = input.next();
-//        System.out.print("Input your Telnumber : ");
-//        telnum = input.next();
->>>>>>> d2b4a4fc42365b5cf7ae78cb549eaac32cc4ba8f
-//    }
+
+
 
     @Override
     public String toString() {
